e278905d747c99c2e4183018d7d6c604c22458b8
DoeringConsulting <a.doering@doering-consulting.eu>
2025-06-09 15:42:15 +0200

    Downloadpfade in DownloadPage.tsx berichtigt

--- src/pages/DownloadsPage.tsx
+++ src/pages/DownloadsPage.tsx
@@ -205,7 +205,7 @@ const DownloadsPage: React.FC = () => {
             <DownloadDescription>
               {t('downloads.projectOverview.description')}
             </DownloadDescription>
-            <DownloadButton href="/downloads/Projektuebersicht_Alexander_Doering.pdf" target="_blank" rel="noopener noreferrer">
+            <DownloadButton href="./downloads/Projektuebersicht_Alexander_Doering.pdf" target="_blank" rel="noopener noreferrer">
               <DownloadButtonIcon>
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
