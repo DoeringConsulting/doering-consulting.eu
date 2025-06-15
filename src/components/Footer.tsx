c8ab4dcde4ff17ae4bff7fde130d600c7b228900
DoeringConsulting <a.doering@doering-consulting.eu>
2025-06-15 22:45:19 +0200

    Cleanup: ungenutztes styled component FooterText entfernt

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -51,32 +51,74 @@ const FooterTitle = styled.h3`
   }
 `;
 
+const FooterList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const FooterListItem = styled.li`
+  margin-bottom: 0.75rem;
+`;
+
+const FooterLink = styled(Link)`
+  color: ${(props) => props.theme.colors.white};
+  text-decoration: none;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: ${(props) => props.theme.colors.gold};
+  }
+`;
+
+const FooterExternalLink = styled.a`
+  color: ${(props) => props.theme.colors.white};
+  text-decoration: none;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: ${(props) => props.theme.colors.gold};
+  }
+`;
+
+const FooterContactItem = styled.div`
+  display: flex;
+  align-items: flex-start;
+  margin-bottom: 1rem;
+
+  svg {
+    margin-right: 0.75rem;
+    flex-shrink: 0;
+    margin-top: 0.2rem;
+  }
+`;
+
 const Footer: FC = () => {
   return (
-    <IconContext.Provider value={{ className: 'mr-2 inline text-white align-middle' }}>
+    <IconContext.Provider value={{ size: '1em' }}>
       <FooterContainer>
         <FooterContent>
           <FooterColumn>
             <FooterTitle>Unternehmen</FooterTitle>
-            <ul>
-              <li><Link to="/about">Über uns</Link></li>
-              <li><Link to="/services">Leistungen</Link></li>
-              <li><Link to="/downloads">Downloads</Link></li>
-              <li><Link to="/kontakt">Kontakt</Link></li>
-            </ul>
+            <FooterList>
+              <FooterListItem><FooterLink to="/about">Über uns</FooterLink></FooterListItem>
+              <FooterListItem><FooterLink to="/services">Leistungen</FooterLink></FooterListItem>
+              <FooterListItem><FooterLink to="/downloads">Downloads</FooterLink></FooterListItem>
+              <FooterListItem><FooterLink to="/kontakt">Kontakt</FooterLink></FooterListItem>
+            </FooterList>
           </FooterColumn>
           <FooterColumn>
             <FooterTitle>Rechtliches</FooterTitle>
-            <ul>
-              <li><Link to="/impressum">Impressum</Link></li>
-              <li><Link to="/datenschutz">Datenschutz</Link></li>
-            </ul>
+            <FooterList>
+              <FooterListItem><FooterLink to="/impressum">Impressum</FooterLink></FooterListItem>
+              <FooterListItem><FooterLink to="/datenschutz">Datenschutz</FooterLink></FooterListItem>
+            </FooterList>
           </FooterColumn>
           <FooterColumn>
             <FooterTitle>Adresse</FooterTitle>
-            <p>
+            <FooterContactItem>
               <FaMapMarkerAlt />
-              <a
+              <FooterExternalLink
                 href="https://www.google.com/maps/search/?api=1&query=D%C3%96RING+Consulting,+ul.+Gen.+Leopolda+Okulickiego+4,+41-208+Sosnowiec,+Poland"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -84,19 +126,21 @@ const Footer: FC = () => {
                 DÖRING CONSULTING<br />
                 ul. Gen. Leopolda Okulickiego 4<br />
                 41-208 Sosnowiec, POLAND
-              </a>
-            </p>
+              </FooterExternalLink>
+            </FooterContactItem>
           </FooterColumn>
           <FooterColumn>
             <FooterTitle>Kontakt</FooterTitle>
-            <p>
+            <FooterContactItem>
               <FaPhoneAlt />
-              <a href="tel:+48123456789">+48 123 456 789</a>
-            </p>
-            <p>
+              <FooterExternalLink href="tel:+48123456789">+48 123 456 789</FooterExternalLink>
+            </FooterContactItem>
+            <FooterContactItem>
               <FaEnvelope />
-              <a href="mailto:kontakt@doering-consulting.eu">kontakt@doering-consulting.eu</a>
-            </p>
+              <FooterExternalLink href="mailto:kontakt@doering-consulting.eu">
+                kontakt@doering-consulting.eu
+              </FooterExternalLink>
+            </FooterContactItem>
           </FooterColumn>
         </FooterContent>
       </FooterContainer>
